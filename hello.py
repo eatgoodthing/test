f3e7a8e536f2fb6af666a6755563b3f6bdb70c30
ron_the_rocker <96737425+superbiscota@users.noreply.github.com>
2022-11-03 01:14:05 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
-print ("Hello github world")
-print ("new function")
-print ("and this is test")
-profile = {"00": "湯浅 大", "0": "増田 大輝", "1": "なし", "2": "吉川　尚輝",
+profile = {
+	"00": "湯浅 大", 
+	"0": "増田 大輝",
+	"1": "なし", 
+	"2": "吉川　尚輝"
+ }
 
+print (profile["0"])
